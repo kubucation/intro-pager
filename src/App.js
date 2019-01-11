78498bd10a5e21dbbbcbd4b0ae906008bad8e5e9
Etienne Dilocker <etienne@dilocker.de>
2019-01-11 20:25:01 +0100

    build default screen, everything hard-coded

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <span className="logo">kubucation</span>
+        <header className="App-header">
+          <h1>Some video title that is awesome</h1>
+          <h2>Some video subtitle that is also awesome and a bit longer so that we have plenty of content to show</h2>
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
